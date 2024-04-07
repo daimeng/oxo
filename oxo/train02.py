55322d2fbad5c544973752a8fa1046f55068ae70
Daimeng Chen <daimengchen@gmail.com>
2024-04-08 02:35:13 +0800

    restore older training

--- oxo/train02.py
+++ oxo/train02.py
@@ -1,32 +1,155 @@
-from game import TicTacToe
-from keras.models import Sequential
-from keras.layers import Dense, Input
-
-# from player import AiPlayer
+import sys
+from numba import njit
 import numpy as np
+from .game import TicTacToe, print_board, pack
+from .player import AiPlayer, Player, RandomPlayer, HumanPlayer
 
-X = []
-y = []
 
-# if __name__ == "__main__":
-#     for i in range(10):
-#         game = TicTacToe(players=[AiPlayer("model.keras"), AiPlayer("model.keras")])
+@njit()
+def train(game: TicTacToe, players: list[Player]):
+    xp: list[tuple[int, int, float]] = [(-1, 0, 0), (-1, 0, 0)]
+    wins0 = 0
+    wins1 = 0
 
-#         while True:
-#             cell = game.curr_player().request_move(game.board, game.left)
-#             X.append(game.board)
-#             y.append(cell)
-#             over = game.make_move(cell)
-#             if over:
-#                 break
+    for _ in range(1000):
+        # if isinstance(players[0], AiPlayer):
+        #     players[0].epsilon -= 0.0002
+        # if isinstance(players[1], AiPlayer):
+        #     players[1].epsilon *= 0.0001
 
-#     X = np.array(X)
-#     y = np.array(y)
+        for _ in range(1000):
+            game.reset()
 
-#     model = Sequential()
-#     model.add(Input(shape=(9,)))
-#     model.add(Dense(9, activation="relu"))
-#     model.add(Dense(1, activation="softmax"))
-#     model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-#     model.fit(X, y, epochs=100, batch_size=64)
-#     model.save("model.keras")
+            done = False
+
+            while not done:
+                pnum = int(game.current_player_id != 1)
+                player = players[pnum]
+
+                state = pack(game.board)
+
+                # update experience before replacing
+                s, a, r = xp[pnum]
+                if s != -1 and isinstance(player, AiPlayer):
+                    player.update(s, a, r, state)
+
+                # get next experience
+                action = player.request_move(game)
+                reward = 0.0
+                match game.make_move(action):
+                    case 3:
+                        reward = -10.0
+                    case 2:
+                        reward = 0.0
+                    case 0:
+                        reward = 0.0
+                        done = True
+                    case x:
+                        reward = 1
+                        if x == 1:
+                            wins0 += 1
+                        elif x == -1:
+                            wins1 += 1
+                        done = True
+
+                xp[pnum] = (state, action, reward)
+
+                # if final state
+                if done:
+                    if isinstance(player, AiPlayer):
+                        player.update(state, action, reward, pack(game.board))
+
+                    # final update for losing player
+                    pnum = int(game.current_player_id != 1)
+                    player = players[pnum]
+                    s, a, r = xp[pnum]
+
+                    if s != -1 and isinstance(player, AiPlayer):
+                        player.update(s, a, -r, state)
+
+    return wins0, wins1
+
+
+def main(opts):
+    game = TicTacToe()
+    players: list[Player] = [AiPlayer(epsilon=0.5), AiPlayer(epsilon=0.5)]
+
+    w0, w1 = train(game, players)
+    print(w0, w1)
+
+    if "play" in opts or "play2" in opts:
+        if "play" in opts:
+            if isinstance(players[0], AiPlayer):
+                players[0].epsilon = 0
+            players: list[Player] = [players[0], HumanPlayer()]
+        else:
+            if isinstance(players[1], AiPlayer):
+                players[1].epsilon = 0
+            players: list[Player] = [HumanPlayer(), players[1]]
+
+        for _ in range(1000):
+            game.reset()
+
+            while True:
+                print_board(game.board)
+                player = players[int(game.current_player_id != 1)]  # 1 -> 0, -1 -> 1
+                cell = player.request_move(game)
+                if game.make_move(cell) < 2:
+                    break
+
+    elif "test" in opts:
+        if isinstance(players[0], AiPlayer):
+            players[0].epsilon = 0
+        players2: list[Player] = [players[0], RandomPlayer()]
+        wins0 = 0
+        wins1 = 0
+        for _ in range(10000):
+            # os.system("clear")
+            game.reset()
+
+            while True:
+                pnum = int(game.current_player_id != 1)
+                player = players2[pnum]
+                action = player.request_move(game)
+                res = game.make_move(action)
+                # os.system("clear")
+                # print(f"Player {pnum} plays {action}!")
+                # print(game.board.reshape(3, 3))
+                if res < 2:
+                    if res == 1:
+                        wins0 += 1
+                    if res == -1:
+                        wins1 += 1
+                    break
+
+        print(wins0, wins1)
+
+        if isinstance(players[1], AiPlayer):
+            players[1].epsilon = 0
+        players2: list[Player] = [RandomPlayer(), players[1]]
+        wins0 = 0
+        wins1 = 0
+        for _ in range(10000):
+            # os.system("clear")
+            game.reset()
+
+            while True:
+                pnum = int(game.current_player_id != 1)
+                player = players2[pnum]
+                action = player.request_move(game)
+                res = game.make_move(action)
+                # os.system("clear")
+                # print(f"Player {pnum} plays {action}!")
+                # print(game.board.reshape(3, 3))
+                if res < 2:
+                    if res == 1:
+                        wins0 += 1
+                    if res == -1:
+                        wins1 += 1
+                    break
+
+        print(wins0, wins1)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
